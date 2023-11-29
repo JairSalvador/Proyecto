22b908a1c9cb6ae44faf86f751569ee51684afaa
JairSalvador <jairsalvador71@aragon.unam.mx>
2023-11-29 16:49:50 -0600

    Se agrega recuerda

---
+++ Inicio de Sesion/Login/home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- interfaz -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recuperar contraseña</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <form method="post">
+        <section class="form-login">
+            <h5>Bienvenido :3</h5>
+            <p></p>
+            <p><a href="\index.php">Cerrar Sesion</a></p>
+            </section>
+    </form>
+    <?php
+    ?>
+</body>
+</html>
---
+++ Inicio de Sesion/Recuerda/recuerdala.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recuperar contraseña</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <form method="post">
+        <section class="form-login">
+            <h5>Recuerdala :3</h5>
+            <p>Estamos trabajando para poder recuperarla</p>
+            <p><a href="index.php">Iniciar Sesion</a></p>
+            </section>
+    </form>
+    <?php
+    ?>
+</body>
+</html>
