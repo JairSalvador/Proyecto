778bc70424dc12d4c1ad524fd5c6ab417da46901
JairSalvador <jairsalvador71@aragon.unam.mx>
2023-11-29 19:29:17 -0600

    se crea la base de datos

---
+++ Inicio de Sesion/Eliminar/db.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE id21595672_registros;
+USE id21595672_registros;
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario VARCHAR(50),
+    contrasena VARCHAR(50)
+);
